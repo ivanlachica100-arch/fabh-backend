9f1a9b5721a82616d7ff86fb3e992342fdeb702f
Roan Cortez <ropa.cortez.up@phinmaed.com>
2026-09-23 21:21:09 +0800

    Set default production API to Render

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://fabh-backend.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
